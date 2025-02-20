62390cf59a0b013da74285c24958ffcd91b695e3
techramj <79793143+techramj@users.noreply.github.com>
2025-02-20 18:59:36 +0530

    dummy_users.ts

---
+++ dummy_users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'user1',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'User 2',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Marcus Johnson',
+    avatar: 'user 3,
+  },
+  {
+    id: 'u4',
+    name: 'user 4',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'user 6',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'user 7',
+    avatar: 'user-6.jpg',
+  },
+];
